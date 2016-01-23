6f2d7cd0624928dceb0679879adfdf1ab6bd2455
vallegro <vallegro@vallegros-MacBook-Pro.local>
2016-01-23 23:50:41 +0900

    bug fix

--- Graph/Node2LapNormal.m
+++ Graph/Node2LapNormal.m
@@ -11,7 +11,7 @@ var_block = max( var(graph_seed(:)), 0.01 );
         
 for i_node = 1:block_total,
     [sub1,sub2] = ind2sub(block_size, i_node);
-    for i_nei = 1:4
+    for i_nei = 1:length(neis)
         sub_nei = [sub1,sub2] + neis(i_nei,:);
         if sub_nei(1)>0 && sub_nei(2)>0 && sub_nei(1)<= block_size(1) && sub_nei(2)<=block_size(2),
             ind_nei = sub2ind( block_size, sub_nei(1),sub_nei(2));
